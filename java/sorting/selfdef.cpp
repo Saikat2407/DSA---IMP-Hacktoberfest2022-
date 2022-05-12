9a1b0060cc029f853290ab381bbe82c11e54b3d3
Saikat Noob <s.nayek2407@gmail.com>
2022-05-12 20:37:35 +0530

    some files Deleted

---
+++ java/sorting/selfdef.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        int count = 0;
+
+        for (int i = 0; i < n; i++){
+            cin>>arr[i];
+        }
+    }
+    return 0;
+}
